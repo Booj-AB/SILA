2c16d2ddc85337c59550483a0d0430a51d5cc287
Sidodido <zidanesidahmed18@gmail.com>
2024-11-02 01:04:36 +0100

    zidane

--- src/components/screens/exposants/exposants.js
+++ src/components/screens/exposants/exposants.js
@@ -13,10 +13,9 @@ import Menu from "./menu";
 
 
 
-
-
 export default function Exposant() {
- 
+  
+
   const [items, setItems] = useState(Menu);
   const filterItem =(categoryItem) =>{
     const updatedItems = Menu.filter((curElem) => {
@@ -27,6 +26,8 @@ setItems(updatedItems);
   }
 
   
+
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: colors.Quaternary}}>
       <View style={{flex: 1, backgroundColor: colors.Quaternary}}>
@@ -50,8 +51,8 @@ setItems(updatedItems);
             <NativeBaseProvider>
              
 
-//kjyuguyg
-           
+{/* //kjyuguyg
+            */}
               <View
                 style={{
                   backgroundColor: colors.Quaternary,
@@ -110,25 +111,52 @@ setItems(updatedItems);
                   </Text>
                 </TouchableOpacity>
 
-               
+             
               </View>
 
 
 
+              {/* <View style={styles.container}>
+                <Pdf
+                    source={{ uri: "http://samples.leanpub.com/thereactnativebook-sample.pdf"}}
+                    onLoadComplete={(numberOfPages,filePath) => {
+                        console.log(`Number of pages: ${numberOfPages}`);
+                    }}
+                    onPageChanged={(page,numberOfPages) => {
+                        console.log(`Current page: ${page}`);
+                    }}
+                    onError={(error) => {
+                        console.log(error);
+                    }}
+                    onPressLink={(uri) => {
+                        console.log(`Link pressed: ${uri}`);
+                    }}
+                    style={styles.pdf}/>
+            </View> */}
 
-
-
-
-              <View
+             
+              {/* <View
                 style={{
                   marginTop: 50* scale,
                   textAlign:'center'
                 }}>
+
+
+
+
+<Pdf trustAllCerts={false} source={{uri: "https://drive.google.com/drive/u/1/folders/1P8-0q8XifQyg17GjkYEhsKiale8F2kYS"}} style={{flex:1,width:Dimensions.get('window').width}}/>
+
+
                   <Text  style={{
                   marginTop: 50* scale,
                   textAlign:'center'
                 }}>hna ndirou des filtres(kaynin)</Text>
-                </View>
+
+
+
+
+
+                </View> */}
 
 
 
@@ -155,3 +183,16 @@ setItems(updatedItems);
       
   );
 }
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      marginTop: 25,
+  },
+  pdf: {
+      flex:1,
+      width:Dimensions.get('window').width,
+      height:Dimensions.get('window').height,
+  }
+});
--- src/components/screens/index.js
+++ src/components/screens/index.js
@@ -15,7 +15,6 @@ export{
   Ouvrage,
   Programmes,
   Exposants,
-  
  
 }
 
