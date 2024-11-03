9e3d33053821111452efcdf9a0a27fde4b7404aa
Sidodido <zidanesidahmed18@gmail.com>
2024-11-03 12:36:56 +0100

    zidane

--- src/components/screens/exposants/menu.js
+++ src/components/screens/exposants/menu.js
@@ -1,31 +1,65 @@
-import sila from '../../../assets/bottomBarIcon/sila.png'
+
+
 
 const menu =[
     {
         id: 1,
         titre:"Guide d'éxposants",
         description:"site web + application pour le departement informatique",
-         image:"sila",
-         category:"Guide",
+        image:require('../../../assets/bottomBarIcon/guide.png'),
+        category:"Guide",
+        link:"https://drive.google.com/drive/u/1/folders/1Aj7nXsVjtr__icYEzfpAuj2qeYaAxREM",
 
         
     },
 
     {
         id: 2,
-        titre:"Plan d'éxposition",
+        titre:"Plan d'éxposition - AHHAGAR -",
         description:"site web + application pour le departement informatique",
-         image:"sila",
+         image:require('../../../assets/bottomBarIcon/ahhagar.png'),
          category:"Plan",
+         link:" ",
+        
+    },
+    {
+        id: 3,
+        titre:"Plan d'éxposition - MEZANNINE -",
+        description:"site web + application pour le departement informatique",
+        image:require('../../../assets/bottomBarIcon/mezannine.png'),
+        category:"Plan",
+        link:" ",
+
+        
+    },
+    {
+        id: 4,
+        titre:"Plan d'éxposition - CASBAH -",
+        description:"site web + application pour le departement informatique",
+        image:require('../../../assets/bottomBarIcon/casbah.png'),
+        category:"Plan",
+        link:" ",
+        
+    },
+    {
+        id: 5,
+        titre:"Plan d'éxposition - CONCOREDE -",
+        description:"site web + application pour le departement informatique",
+        image:require('../../../assets/bottomBarIcon/concorde.png'),
+        category:"Plan",
+        link:" ",
+
         
     },
 
     {
-        id: 3,
+        id: 6,
         titre:"Réglements interieurs",
         description:"site web + application pour le departement informatique",
-         image:"sila",
-         category:"Reglements",
+        image:require('../../../assets/bottomBarIcon/reglements.png'),
+        category:"Reglements",
+        link:" ",
+
 
         
     },
